a341f4ffa5ea0669503869f701f2e19c18e983cf
Elias Brown <eli.marc.brown@gmail.com>
2026-07-04 00:28:32 +0200

    Added player classes

---
+++ Source/SpicyBlueb/Player/PZ_PlayerCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PZ_PlayerCharacter.h"
+
+
+// Sets default values
+APZ_PlayerCharacter::APZ_PlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APZ_PlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APZ_PlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APZ_PlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/SpicyBlueb/Player/PZ_PlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PZ_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPICYBLUEB_API APZ_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
