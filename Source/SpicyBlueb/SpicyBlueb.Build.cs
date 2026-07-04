6e278f6ab6475b9c4b796d5764a06487dfa73b28
Elias Brown <eli.marc.brown@gmail.com>
2026-07-04 17:13:52 +0200

    Fixed editor modules in wrong place

--- Source/SpicyBlueb/SpicyBlueb.Build.cs
+++ Source/SpicyBlueb/SpicyBlueb.Build.cs
@@ -8,9 +8,14 @@ public class SpicyBlueb : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "PCG", "PhysicsControl", "PhysicsControlUncookedOnly", "PhysicsControlEditor" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "PCG", "PhysicsControl" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "NetCore", "Slate", "SlateCore"  });
+
+		if (Target.Type == TargetType.Editor)
+		{
+			PrivateDependencyModuleNames.AddRange(new string[] { "PhysicsControlUncookedOnly", "PhysicsControlEditor"});
+		}
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
