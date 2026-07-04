d3d9635e240040bc0b085f12dfee6cdc9aa7acfe
Elias Brown <eli.marc.brown@gmail.com>
2026-07-04 14:15:32 +0200

    Added two-player multiplayer setup

--- Source/SpicyBlueb/GameMode/PZ_GameModeBase.cpp
+++ Source/SpicyBlueb/GameMode/PZ_GameModeBase.cpp
@@ -8,5 +8,5 @@ APZ_GameModeBase::APZ_GameModeBase()
 void APZ_GameModeBase::BeginPlay()
 {
 	Super::BeginPlay();
-	UGameplayStatics::CreatePlayer(this, 1, true);
+	//UGameplayStatics::CreatePlayer(this, 1, true);
 }
