788d59bfdf131d55764d6ae68a1f80cb8cae098b
Birkan Ates <Billy.ates@hotmail.com>
2026-07-04 02:09:49 +0200

    PlayerCharacter input working

--- Source/SpicyBlueb/Player/PZ_PlayerCharacter.cpp
+++ Source/SpicyBlueb/Player/PZ_PlayerCharacter.cpp
@@ -3,30 +3,87 @@
 
 #include "PZ_PlayerCharacter.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "PZ_PlayerController.h"
+#include "Camera/CameraComponent.h"
+#include "Engine/LocalPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
 
-// Sets default values
+
 APZ_PlayerCharacter::APZ_PlayerCharacter()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = CameraHeight;
+	SpringArm->SetRelativeRotation(FRotator(CameraPitch, CameraYaw, 0.f));
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 10.f;
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
 }
 
-// Called when the game starts or when spawned
 void APZ_PlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	AddInputMapping();
 }
 
-// Called every frame
-void APZ_PlayerCharacter::Tick(float DeltaTime)
+void APZ_PlayerCharacter::PawnClientRestart()
 {
-	Super::Tick(DeltaTime);
+	Super::PawnClientRestart();
+	AddInputMapping();
 }
 
-// Called to bind functionality to input
 void APZ_PlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	
+	if (!EIC) return;
+	if (MoveAction) EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APZ_PlayerCharacter::Move);
+	if (JumpAction)
+	{
+		EIC->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EIC->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+	}
+	
 }
 
+void APZ_PlayerCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D Axis = Value.Get<FVector2D>();
+	
+	const FRotator YawRot(0.f, SpringArm->GetComponentRotation().Yaw, 0.f);
+	const FVector Forward = FRotationMatrix(YawRot).GetUnitAxis(EAxis::X);
+	const FVector Right = FRotationMatrix(YawRot).GetUnitAxis(EAxis::Y);
+	
+	AddMovementInput(Forward, Axis.Y);
+	AddMovementInput(Right, Axis.X);
+}
+
+void APZ_PlayerCharacter::AddInputMapping()
+{
+	if (APZ_PlayerController* PC = Cast<APZ_PlayerController>(GetController()))
+	{
+		if (ULocalPlayer* LP = PC->GetLocalPlayer())
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* EILPS = LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+			{
+				if (DefaultMappingContext)
+				{
+					EILPS->AddMappingContext(DefaultMappingContext, 0);
+				}
+			}
+		}
+	}
+}
--- Source/SpicyBlueb/Player/PZ_PlayerCharacter.h
+++ Source/SpicyBlueb/Player/PZ_PlayerCharacter.h
@@ -3,8 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
 #include "PZ_PlayerCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class SPICYBLUEB_API APZ_PlayerCharacter : public ACharacter
@@ -12,17 +18,43 @@ class SPICYBLUEB_API APZ_PlayerCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	APZ_PlayerCharacter();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void PawnClientRestart() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	
+	// Camera
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	USpringArmComponent* SpringArm;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent* Camera;
+	
+	// Inputs
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* JumpAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Feel|Camera")
+	float CameraHeight = 1500.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Feel|Camera")
+	float CameraPitch = -55.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Feel|Camera")
+	float CameraYaw = 45.f;
+	
+	// Input handlers
+	void Move(const FInputActionValue& Value);
+	void AddInputMapping();
+	
+private:
+	
 };
