5f7d97c87fe82954608b319d72cccbb31a8cb185
Birkan Ates <Billy.ates@hotmail.com>
2026-07-04 15:20:08 +0200

    Build and uproject PhysicsControl bug

--- Source/SpicyBlueb/SpicyBlueb.Build.cs
+++ Source/SpicyBlueb/SpicyBlueb.Build.cs
@@ -8,9 +8,9 @@ public class SpicyBlueb : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "PCG", "PhysicsControl", "PhysicsControlUncookedOnly", "PhysicsControlEditor"});
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "NetCore"  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "NetCore"});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
